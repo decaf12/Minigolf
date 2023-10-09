932a424281c66eef7cc5851533d2bb68dcc02024
Siyang He <siyang.he.uw@gmail.com>
2023-10-08 22:59:40 -0400

    power slider logic

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
     public float maxPower;
     public float changeAngleSpeed;
     public float lineLength;
+    public Slider powerSlider;
 
     private LineRenderer line;
     private Rigidbody ball;
     private float angle;
+    private float powerUpTime;
+    private float power;
 
     void Awake()
     {
@@ -20,6 +24,8 @@ public class BallController : MonoBehaviour
            The default is too low. Raise it to 1000. */
         ball.maxAngularVelocity = 1000; 
         line = GetComponent<LineRenderer>();
+        powerUpTime = 0;
+        powerSlider.value = 0;
     }
 
     void Update()
@@ -35,6 +41,10 @@ public class BallController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Space))
         {
             Putt();
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            PowerUp();
         }
         UpdateLinePositions();
     }
@@ -52,6 +62,15 @@ public class BallController : MonoBehaviour
 
     private void Putt()
     {
-        ball.AddForce(Quaternion.Euler(0, angle, 0) * Vector3.forward * maxPower, ForceMode.Impulse);
+        ball.AddForce(Quaternion.Euler(0, angle, 0) * Vector3.forward * maxPower * power, ForceMode.Impulse);
+        powerUpTime = 0;
+        powerSlider.value = 0;
+    }
+
+    private void PowerUp()
+    {
+        powerUpTime += Time.deltaTime;
+        power = Mathf.PingPong(powerUpTime, 1);
+        powerSlider.value = power;
     }
 }
